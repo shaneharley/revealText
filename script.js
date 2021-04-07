73fb6697cdbd04ecd1399037a0e31228331c3e07
Shane Harley <shaneharley@gmail.com>
2021-04-07 08:03:20 -0700

    Added comments

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
+//getting all the menu items
 let menuItems = document.querySelectorAll('.menuItem .innerText')
 
-console.log(menuItems)
-
+//this creates a delay that resets the animation delay
 function resetDelay(delay) {
   delay.style.transitionDelay = '0s'
 }
 
 window.addEventListener('click', () => {
-
   for (let i = 0; i < menuItems.length; i++) {
+    //increasing each delay per menuItem
     let animationDelay = 0 + (i / 5)
+
+    //setting the transition delay
     menuItems[i].style.transitionDelay = animationDelay + 's'
+
+    //toggling the class
     menuItems[i].classList.toggle('animated')
+
+    //reseting the transition delay
     setTimeout(function () {
       resetDelay(menuItems[i])
     }, (i + (i * 1000)));
